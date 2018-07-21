5fbddb7f21435270b8777b752c358c89fce08d66
test <test@tests-MacBook.local>
2018-07-21 11:39:30 +0700

    initial commit

--- FlaskIntro/homework_session08/ex1.py
+++ FlaskIntro/homework_session08/ex1.py
@@ -5,13 +5,11 @@ app = Flask(__name__)
 
 @app.route("/") 
 def index(): 
-    return "Lets go to CHRONO SHOWWWW"
+    return "helo"
 
 @app.route("/about-me") 
 def about_me():
-    return '''ĐI CHRONO ĐI NHÉ Ạ SHOW ÂM NHẠC VỚI HISAM, PHƯƠNG LY VÀ VÔ VÀN KHÁCH MỜI KHÁC.
-    AI CHẤM BÀI EM THÌ GHÉ FANPAGE CNNSHINE TRÊN FACEBOOK NHÉ Ạ. CƠ HỘI NGÀN LẦN CÓ MỘT ĐỂ GẶP PHƯƠNG LY ĐÓ
-    SIÊU ĐỈNH UHUHU ONCE IN A LIFE TIME. GRAB YOUR CHANCE!!!!!!!''' 
+    return '''my name is phương anh''' 
 
 @app.route("/school")
 def school(): 
